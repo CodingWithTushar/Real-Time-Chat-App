fd9ef7e127b655daf00cf5d5151d58dfd7ea2606
Tushar <156307843+CodingWithTushar@users.noreply.github.com>
2025-03-12 20:31:04 +0530

    Update index.js

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const port = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "200mb" }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
